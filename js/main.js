0abece72d39b22e43f3efdf9e76e4331ea891ff8
NastyaSvastya <nastya.a.goodlocal@gmail.com>
2021-02-12 09:58:06 +0300

    поменяла let на const и добавила проверки

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-let randomInteger = (min, max) => {
+const randomInteger = (min, max) => {
+  if (min <0 || max < 0 || min > max) {
+  throw new Error();
+} else{
   let rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
+  return Math.floor(rand);}
 };
-let randomFractional = (min, max, round) => {
+const randomFractional = (min, max, round) => {
+  if (min <0 || max < 0 || min > max) {
+  throw new Error();
+} else{
   let rand = min + Math.random() * (max - min);
-  return rand.toFixed(round);
+  return rand.toFixed(round);}
 };
